9d3b44863d4a9d1c24c53a3a1b568a7437b0f456
Reinan Kauã <contato.reinankaua@gmail.com>
2022-05-14 23:13:10 -0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css'
+import Calculadora from './components/Calculator'
+
+function App() {
+  return (
+    <div className="App">
+     <Calculadora/>
+    </div>
+  )
+}
+
+export default App
